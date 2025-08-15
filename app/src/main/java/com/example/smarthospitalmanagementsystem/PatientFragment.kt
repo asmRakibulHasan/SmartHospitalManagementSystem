007b6f1aa822617c397d63a73eabea3f31de91a4
myself <myself@gmail.com>
2025-08-16 05:56:28 +0600

    - prescription screen implemented

--- app/src/main/java/com/example/smarthospitalmanagementsystem/PatientFragment.kt
+++ app/src/main/java/com/example/smarthospitalmanagementsystem/PatientFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.smarthospitalmanagementsystem.adapter.Doctor
 import com.example.smarthospitalmanagementsystem.adapter.DoctorAdapter
@@ -51,7 +52,7 @@ class PatientFragment : Fragment() {
         )
 
         doctorAdapter = DoctorAdapter(doctors) { doctor ->
-            // Handle Book Now click
+            findNavController().navigate(R.id.action_patientFragment_to_prescriptionFragment)
         }
         binding.recyclerDoctors.adapter = doctorAdapter
         binding.recyclerDoctors.layoutManager = LinearLayoutManager(requireContext())
